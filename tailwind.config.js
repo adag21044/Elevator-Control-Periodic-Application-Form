e1ff029f6e91f287492070603dcac55be1fbde09
Ahmet E. Dag <adag21044@gmail.com>
2024-07-10 11:30:02 +0300

    INITIALIZE

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontWeight: {
+        semibold: 600, // Add semibold with a value of 600
+      },
+    },
+  },
+  plugins: [],
+}
